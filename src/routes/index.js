faaa5b2eba1e424dfa2af63bc19a76a6d1e8608d
Duarte <duartedesousa.contato@gmail.com>
2026-04-14 14:39:17 -0300

    RoutesCompleto e AlteracoesIndexEScript

--- src/routes/index.js
+++ src/routes/index.js
@@ -165,7 +165,43 @@ router.delete('/pedidos/:id', auth, async (req, res) => { // Rota que deletea um
 router.get('/usuarios', auth, async (req, res) => { // Rota que coleta e mostra todos os usuários (acesso apenas para administradores)
     try {
         if (req.usuario.perfil !== 'Administrador')
-            return res.status(403)
-    }
+            return res.status(403).json({ erro: 'Acesso restrito a Administradores'});
+        res.json(await Usuario.findAll());
+    } catch (e) { res.status(500).json({ erro: e.messgae}); }
+});
 
-})
+router.post('/usuarios', auth, async (req, res) => {
+    try {
+        if (req.usuario.perfil !== 'Administrador')
+            return res.status(403).json({ erro: 'Acesso restrito a Administradores'});
+        const { nome, email, senha, perfil } = req.body;
+        if(!nome || !email || !senha)
+            return res.status(400).json({ erro: 'Nome, email e senha são obrigatórios'});
+        res.status(201).json(await Usuario.create({nome, email, senha, perfil}));
+    } catch (e) {
+        if (e.message?.includes('UNIQUE')) return res.status(400).json({ erro: 'E-mail já cadastrado'});
+        res.status(500).json({ erro: e.message});
+    }
+});
+
+router.put('/usuarios/:id', auth, async (req, res) => { // Rota que pesquisa usuarios pelo id (acesso apenas para administradores)
+    try {
+        if (req.usuario.perfil !== 'Administrador')
+            return res.status(403).json({ erro: 'Acesso restrito a Administradores'});
+        const u = await Usuario.update(req.params.id, req.body);
+        if (!u) return res.status(404).json({ erro: e.message});
+        res.json(u);
+    } catch (e) { res.status(500).json({erro: e.message}); }
+});
+
+router.delete('/usuarios/:id', auth, async (req, res ) => { // Rota que deleta um usuário existente (acesso apenas para administradores)
+    try {
+        if (req.usuario.perfil !== 'Administrador')
+            return res.status(403).json({ erro: 'Acesso restrito a Administradores'});
+        const ok = await Usuario.delete(req.params.id);
+        if (!ok) return res.status(404).json({ erro: 'Usuário não encontrado'});
+        res.json({ mensagem: 'Usuário deletado'});
+    } catch (e) { res.status(500).json({ erro: e.message});}
+});
+
+module.exports = router; // Módulo que exportará as rotas
