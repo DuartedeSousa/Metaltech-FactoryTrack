c55e766b1d3c7c8fad4631fa7623e77626745156
Duarte <duarte.desousa.contato@gmail.com>
2026-05-18 14:03:09 -0300

    index-java

--- GitAutoSave.bat
+++ GitAutoSave.bat
@@ -2,7 +2,7 @@ git status
 git config --global user.name "Duarte"
 git config --global user.email duarte.desousa.contato@gmail.com
 git add .
-git commit -m "Models"
+git commit -m "index-java"
 git branch -M main
 git remote add origin https://github.com/DuartedeSousa/Metaltech-FactoryTrack.git
 git push -u origin main
