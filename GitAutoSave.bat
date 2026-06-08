9209324aec4273cfd794f871d0476871ba53e666
joao <joao.bacurau@aluno.senai.br>
2026-06-08 11:35:38 -0300

    erros-resolvidos(apoio)_X

--- GitAutoSave.bat
+++ GitAutoSave.bat
@@ -1,6 +1,6 @@
 git status
-git config --global user.name "Duarte"
-git config --global user.email duarte.desousa.contato@gmail.com
+git config --global user.name "joao"
+git config --global user.email joao.bacurau@aluno.senai.br
 git add .
 git commit -m "erros-resolvidos(apoio)_X"
 git branch -M main
